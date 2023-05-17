b27f89ff7ec3369b2c46d200fa560584d768f523
Pedro Zenere <pedro.zenere@sydy.com.br>
2023-05-16 22:29:40 -0400

    Ajusta estrutura de pastas do projeto. Configurando DbContext

---
+++ src/GuiaEmpresarialAPI.Data/Context/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using GuiaEmpresarialAPI.Data.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuiaEmpresarialAPI.Data.Context
+{
+    public class ApplicationContext : DbContext, IApplicationContext
+    {
+        /// <summary>
+        /// Definindo context
+        /// </summary>
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        { 
+        }
+    }
+}
