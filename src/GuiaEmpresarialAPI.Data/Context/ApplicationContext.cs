8507a801530bab3b0f2bf3ef5fabc47c3896b847
Pedro Zenere <pedro.zenere@sydy.com.br>
2023-05-19 11:18:53 -0400

    Configura comunicacao com sqlserver

--- src/GuiaEmpresarialAPI.Data/Context/ApplicationContext.cs
+++ src/GuiaEmpresarialAPI.Data/Context/ApplicationContext.cs
@@ -3,13 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GuiaEmpresarialAPI.Data.Context
 {
-    public class ApplicationContext : DbContext, IApplicationContext
+    public partial class ApplicationContext : DbContext, IApplicationContext
     {
         /// <summary>
         /// Definindo context
         /// </summary>
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
         }
     }
 }
---
+++ src/GuiaEmpresarialAPI.Data/Services/ConfigurationServices.cs
@@ -0,0 +1,33 @@
+﻿using GuiaEmpresarialAPI.Data.Context;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace GuiaEmpresarialAPI.Data.Services
+{
+    public static class ConfigurationServices
+    {
+        public static void ConfigureMainDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContextPool<ApplicationContext>(options =>
+            {
+                string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                SqlConnectionStringBuilder connectionStringBuilder = new(connectionString);
+
+                options.UseSqlServer(
+                    connectionStringBuilder.ConnectionString,
+                    sqlOptions => sqlOptions.MigrationsHistoryTable("__MigrationHistory")
+                );
+
+                options
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors()
+                    .UseLoggerFactory(LoggerFactory.Create(builder => { builder.AddConsole(); }));
+            });
+        }
+    }
+}
