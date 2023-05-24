86b7d597353d82a96a168ecb371e87011dd7338f
Pedro Zenere <pedro.zenere@sydy.com.br>
2023-05-23 21:58:23 -0400

    Adicionando controler de get de categorias com filtros. Adicionando paginacao na aplicacao

---
+++ src/GuiaEmpresarialAPI.Shared/Core/Queries/GetByFilterQueryBase.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+
+namespace GuiaEmpresarialAPI.Shared.Core.Queries
+{
+    public abstract class GetByFilterQueryBase
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+
+        public string[] OrderBy { get; set; }
+
+        protected GetByFilterQueryBase()
+        {
+            Page = 1;
+            PageSize = 10;
+
+            OrderBy = Array.Empty<string>();
+        }
+    }
+}
---
+++ src/GuiaEmpresarialAPI.Shared/Core/Utils/PaginatedLists/IPaginatedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiaEmpresarialAPI.Shared.Core.Utils.PagedList
+{
+    public interface IPaginatedList<out T>
+    {
+        public int TotalCount { get; }
+        public int PageCount { get; }
+        public int PageSize { get; }
+        public int Page { get; }
+        public T[] Items { get; }
+    }
+}
