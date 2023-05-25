22356698c440289c79dc0043fccaf8a940fc67ef
Pedro Zenere <pedro.zenere@sydy.com.br>
2023-05-25 14:42:08 -0400

    Adicionando migration da tabela Estado

---
+++ src/GuiaEmpresarialAPI.Data/Migrations/20230525183348_AdicionaTabelaEstado.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuiaEmpresarialAPI.Data.Migrations
+{
+    public partial class AdicionaTabelaEstado : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sigla = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estados", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estados");
+        }
+    }
+}
